e1d2d904e4e543bf8e003fe7f978f8a01ceb4811
jingye chen <jich2601@student.miun.se>
2026-09-24 13:30:25 +0200

    Skapade variabler och utskrift i uppgift 2

--- uppgift2.js
+++ uppgift2.js
@@ -1,2 +1,12 @@
 /* Lösning till Uppgift 2. Av Jingye Chen, 2026 */
 "use strict";
+
+let pris = 75;
+let number = 5;
+let total = pris * number;
+let totalIncludingVAT = total * 1.25;
+
+console.log(`Pris: ${pris}`)
+console.log(`Antal: ${number}`)
+console.log(`Total: ${total}`)
+console.log(`Totalt inklusive moms: ${totalIncludingVAT}`)
