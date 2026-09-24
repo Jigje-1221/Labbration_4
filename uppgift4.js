8b47dad7c02b7839dff7ffeb74693b4136efaacd
jingye chen <jich2601@student.miun.se>
2026-09-24 14:24:02 +0200

    skapade en for-loop som skriver ut siffror 1 till 20 sedan ändrade det så att det skriver bara jämn tal mellan 1 till 20

--- uppgift4.js
+++ uppgift4.js
@@ -1,2 +1,15 @@
 /* Lösning till Uppgift 4. Av Jingye Chen, 2026 */
 "use strict";
+
+for(let i = 1; i <= 20; i++){
+    console.log(i);    
+}
+
+console.log("Efter ändring");
+
+
+for(let i = 1; i <= 20; i++){
+    if(i % 2 == 0){
+        console.log(i);    
+    }  
+}
