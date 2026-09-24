bd43b7086c688ba82ba57b78fdc829b49464ff18
jingye chen <jich2601@student.miun.se>
2026-09-24 14:46:32 +0200

    Skapade array for uppgift 5 och skriver ut de, lägg till också en en element i array och tåg bort den första element och skrivde ut den nya array

--- uppgift5.js
+++ uppgift5.js
@@ -1,2 +1,18 @@
 /* Lösning till Uppgift 5. Av Jingye Chen, 2026 */
 "use strict";
+
+let vegetables = ["Broccoli", "Gurka", "Svamp", "Tomat", "Potatis"]
+
+for(let i = 0; i < vegetables.length; i++){
+    console.log(vegetables[i]);
+}
+
+console.log(`Första grönsaker ${vegetables[0]}`);
+console.log(`Sista grönsaker ${vegetables[vegetables.length - 1]}`);
+
+vegetables.push("paprika")
+vegetables.shift()
+
+for(let i = 0; i < vegetables.length; i++){
+    console.log(vegetables[i]);
+}
