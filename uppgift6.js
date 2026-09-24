177ba8cec1f4ec8f34f0ec43d13496fc95e480f0
jingye chen <jich2601@student.miun.se>
2026-09-24 14:53:15 +0200

    Skapade en funktion för areaberäkning i uppgift 6

--- uppgift6.js
+++ uppgift6.js
@@ -1,2 +1,12 @@
 /* Lösning till Uppgift 6. Av Jingye Chen, 2026 */
 "use strict";
+
+function calculateArea(height, width){
+    let sum;
+    sum = height * width;
+    return sum; 
+}
+
+console.log("Arean är: " + calculateArea(10, 5));
+console.log("Arean är: " + calculateArea(40, 10));
+console.log("Arean är: " + calculateArea(20, 3));
