a11378d4e2c2d5d98543cbf2a92f665aedcbfadb
jingye chen <jich2601@student.miun.se>
2026-09-25 09:40:54 +0200

    Lade till en lista av objekt och utskrift av person information i uppgift 9

--- uppgift9.js
+++ uppgift9.js
@@ -1,2 +1,33 @@
 /* Lösning till Uppgift 9. Av Jingye Chen, 2026 */
 "use strict";
+
+const people =[
+    {
+        name: "Jingye",
+        age: 20,
+        city: "Stockholm",
+    },
+    {
+        name: "Markus",
+        age: 43,
+        city: "Uppsala",
+    },
+    {
+        name: "Lucas",
+        age: 14,
+        city: "Östersund",
+    }
+];
+
+function ageChecker (people){
+    for(let i = 0; i < people.length; i++){
+        if(people[i].age < 18){
+            console.log(`${people[i].name} bor i ${people[i].city} och är inte myndig`)
+        }
+        else{
+            console.log(`${people[i].name} bor i ${people[i].city} och är myndig`)
+        }
+    }
+}
+
+ageChecker(people);
