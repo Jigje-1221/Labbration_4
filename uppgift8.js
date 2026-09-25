6e0479a862e670cd079a34ce1ce1e8a959415fec
jingye chen <jich2601@student.miun.se>
2026-09-25 09:15:58 +0200

    Lade till objekt och utskrift av bokinformation i uppgift 8

--- uppgift8.js
+++ uppgift8.js
@@ -1,2 +1,16 @@
 /* Lösning till Uppgift 8. Av Jingye Chen, 2026 */
 "use strict";
+
+let book = {
+    titel: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    yearOfPublication: 1937
+}
+
+function bookInfo(book){
+    console.log("Title: " + book.titel);
+    console.log("Författare: " + book.author);
+    console.log("utgivningsår: " + book.yearOfPublication);
+}
+
+bookInfo(book);
